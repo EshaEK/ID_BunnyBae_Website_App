ccf0b190ba234590f71f9e5466ebf734e7694dff
EEK20 <ekesha02@gmail.com>
2023-02-10 11:15:58 +0800

    adding in menu

--- js/app.js
+++ js/app.js
@@ -192,3 +192,19 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+
+// ======================== Hamburger Menu ========================\
+
+var hamburger = document.getElementById("lines-icon");
+var menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+  } else {
+    menu.classList.add("showMenu");
+  }
+}
