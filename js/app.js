ff14f6d1201c075ea98fe5afe766700b7bc3a748
EEK20 <ekesha02@gmail.com>
2023-02-17 21:36:37 +0800

    changed the link and API key
    Co-authored-by: ainiinpain <ainiinpain@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -1,155 +1,158 @@
-//API function
-$(document).ready(function () {
-    const APIKEY = "63e4b29b478852088da67f10";
-    getContacts();
-    $("#update-contact-container").hide();
-  
-    $("#contact-submit").on("click", function (e) { 
-      e.preventDefault();
-  
+//api function
+$(document).ready(function() {
+  const APIKEY = "63b78dd0969f06502871ab17";
+  getContacts();
+  $("#update-contact-container").hide();
+  $("#add-update-password").hide();
 
-      let contactName = $("#contact-name").val();
-      let contactEmail = $("#contact-email").val();
-      let contactPassword =$("contact-password").val();
-  
 
-      let jsondata = {
-        "name": contactName,
-        "email": contactEmail,
-        "password": contactPassword, // put comma for test first
-      };
-  
-      let settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://projectip-f606.restdb.io/rest/contacts",
-        "method": "POST", 
-        "headers": {
-          "content-type": "application/json",
-          "x-apikey": APIKEY,
-          "cache-control": "no-cache"
-        },
-        "processData": false,
-        "data": JSON.stringify(jsondata),
-        "beforeSend": function(){
-          $("#contact-submit").prop( "disabled", true);
-          $("#add-contact-form").trigger("reset");
-        }
+  $("#contact-submit").on("click", function(e) {
+    e.preventDefault();
+
+
+    let contactName = $("#contact-name").val();
+    let contactEmail = $("#contact-email").val();
+    let contactPassword = $("#contact-password").val();
+
+
+    let jsondata = {
+      "name": contactName,
+      "email": contactEmail,
+      "Password": contactPassword
+    };
+
+ 
+    let settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://interactivedev-4e2a.restdb.io/rest/contact",
+      "method": "POST", 
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+      },
+      "processData": false,
+      "data": JSON.stringify(jsondata),
+      "beforeSend": function() {
+
+        $("#contact-submit").prop("disabled", true);
+        $("#add-contact-form").trigger("reset");
       }
-  
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-        
-        $("#contact-submit").prop( "disabled", false);
-        
-        //@TODO update frontend UI 
-        $("#add-update-msg").show().fadeOut(3000);
-  
-        //update our table 
-        getContacts();
-      });
-    });//end click 
-  
-  
-    //[STEP] 6
-    //let's create a function to allow you to retrieve all the information in your contacts
-    //by default we only retrieve 10 results
-    function getContacts(limit = 10, all = true) {
-  
-      //[STEP 7]: Create our AJAX settings
-      let settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://projectip-f606.restdb.io/rest/contacts",
-        "method": "GET",
-        "headers": {
-          "content-type": "application/json",
-          "x-apikey": APIKEY,
-          "cache-control": "no-cache"
-        },
-      }
-  
-
-      $.ajax(settings).done(function (response) {
-        
-        let content = "";
-  
-        for (var i = 0; i < response.length && i < limit; i++) {
-
-          content = `${content}<tr id='${response[i]._id}'><td>${response[i].name}</td>
-          <td>${response[i].email}</td>
-          <td>${response[i].password}</td>
-          <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' class='update' data-id='${response[i]._id}' data-name='${response[i].name}' data-email='${response[i].email}' data-password=' ${response[i].password}'>Update</a></td></tr>`;
-  
-        }
-
-        $("#create_account").html(content);
-  
-        $("#total-contacts").html(response.length);
-      });
-  
-  
     }
-  
-   
-    $("#contact-list").on("click", ".update", function (e) {
-      e.preventDefault();
-      //update our update form values
-      let contactName = $(this).data("name");
-      let contactEmail = $(this).data("email");
-      let contactPassword = $(this).data("password");
-      let contactId = $(this).data("id");
-    
-  
- 
-      $("#update-contact-name").val(contactName);
-      $("#update-contact-email").val(contactEmail);
-      $("#update-contact-Id").val(contactId);
-      $("update-contact-password").val(contactPassword);
-      $("#update-contact-container").show();
-  
-    });
-    $("#update-contact-submit").on("click", function (e) {
-      e.preventDefault();
-      let contactName = $("#update-contact-name").val();
-      let contactEmail = $("#update-contact-email").val();
-      let contactPassword = $("update-contact-password").val();
 
-      let contactId = $("#update-contact-id").val();
-  
+    $.ajax(settings).done(function(response) {
+      console.log(response);
+
+      $("#contact-submit").prop("disabled", false);
+
+      $("#add-update-password").show().fadeOut(3000);
  
-      updateForm(contactId, contactName, contactEmail,contactPassword);
+      getContacts();
     });
-  
-    function updateForm(id, contactName, contactEmail) {
+  });
+
+
+
+  function getContacts(limit = 10, all = true) {
+
  
-  
-      var jsondata = { "name": contactName, "email": contactEmail , "password":contactPassword};
-      var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": `https://projectip-f606.restdb.io/rest/contacts/${id}`,
-        "method": "PUT",
-        "headers": {
-          "content-type": "application/json",
-          "x-apikey": APIKEY,
-          "cache-control": "no-cache"
-        },
-        "processData": false,
-        "data": JSON.stringify(jsondata)
+    let settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://interactivedev-4e2a.restdb.io/rest/contact",
+      "method": "GET", 
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+      },
+    }
+
+
+    $.ajax(settings).done(function(response) {
+
+      let content = "";
+
+      for (var i = 0; i < response.length && i < limit; i++) {
+    
+        content = `${content}<tr id='${response[i]._id}'><td>${response[i].name}</td>
+        <td>${response[i].email}</td>
+        <td>${response[i].password}</td>
+        <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' class='update' data-id='${response[i]._id}' data-password='${response[i].password}' data-name='${response[i].name}' data-email='${response[i].email}'>Update</a></td></tr>`;
+
       }
+
   
-      //[STEP 13a]: send our AJAX request and hide the update contact form
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-        
-        $("#update-contact-container").fadeOut(5000);
-        //update our contacts table
-        getContacts();
-      });
-    }//end updateform function
-  
-  })
+      $("#contact-list tbody").html(content);
+
+      $("#total-contacts").html(response.length);
+    });
+
+
+  }
+
+  $("#contact-list").on("click", ".update", function(e) {
+    e.preventDefault();
+    
+    let contactName = $(this).data("name");
+    let contactEmail = $(this).data("email");
+    let contactPassword = $(this).data("password");
+    let contactId = $(this).data("id");
+    console.log($(this).data("password"));
+
+
+    $("#update-contact-name").val(contactName);
+    $("#update-contact-email").val(contactEmail);
+    $("#update-contact-password").val(contactPassword);
+    $("#update-contact-id").val(contactId);
+    $("#update-contact-container").show();
+
+  });
+
+
+  $("#update-contact-submit").on("click", function(e) {
+    e.preventDefault();
+    let contactName = $("#update-contact-name").val();
+    let contactEmail = $("#update-contact-email").val();
+    let contactPassword = $("#update-contact-password").val();
+    let contactId = $("#update-contact-id").val();
+
+    console.log($("#update-contact-password").val());
+    console.log(contactPassword);
+
+
+    updateForm(contactId, contactName, contactEmail, contactPassword);
+  });
+
+  function updateForm(id, contactName, contactEmail, contactPassword) {
+   
+
+    var jsondata = { "name": contactName, "email": contactEmail, "password": contactPassword};
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": `https://interactivedev-4e2a.restdb.io/rest/contact/${id}`,
+      "method": "PUT",
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+      },
+      "processData": false,
+      "data": JSON.stringify(jsondata)
+    }
+
+    $.ajax(settings).done(function(response) {
+      console.log(response);
+
+      $("#update-contact-container").fadeOut(5000);
+   
+      getContacts();
+    });
+  }
+
+})
   
 
 // ======================== Light & Dark Mode ========================\
@@ -157,22 +160,6 @@ $(document).ready(function () {
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-}
-
-
-// ======================== Hamburger Menu ========================\
-
-var hamburger = document.getElementById("lines-icon");
-var menu = document.getElementById("menu");
-
-hamburger.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-  } else {
-    menu.classList.add("showMenu");
-  }
 }
 
 // ======================== Lottie Animation ========================\
